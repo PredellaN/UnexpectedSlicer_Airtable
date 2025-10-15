c7a2f41088c151590b40226450e853c1a9db7be9
Nicolas Predella <nicolas@predella.me>
2025-10-15 17:59:04 +0300

    manage constants missing

--- preferences/prefs.py
+++ preferences/prefs.py
@@ -1,7 +1,17 @@
 from bpy.types import AddonPreferences
 import bpy
 from ..registry import register_class
-from .. import PACKAGE, constants
+from .. import PACKAGE
+
+try:
+    from .. import constants
+except ImportError:
+    class _Fallback:
+        AT_PAT = ""
+        AT_BASE_ID = ""
+        AT_ORDERS_TABLE_ID = ""
+    constants = _Fallback()
+
 
 @register_class
 class Slicer_AirtablePreferences(AddonPreferences):
@@ -9,20 +19,21 @@ class Slicer_AirtablePreferences(AddonPreferences):
 
     airtable_pat: bpy.props.StringProperty(
         name="Airtable PAT", default=getattr(constants, "AT_PAT", "")
-    ) #type: ignore
+    )  # type: ignore
     airtable_base: bpy.props.StringProperty(
         name="Airtable Base", default=getattr(constants, "AT_BASE_ID", "")
-    ) #type: ignore
+    )  # type: ignore
     orders_table: bpy.props.StringProperty(
         name="Orders table ID", default=getattr(constants, "AT_ORDERS_TABLE_ID", "")
-    ) #type: ignore
+    )  # type: ignore
 
     def draw(self, context):
         layout = self.layout
         for prop in ("airtable_pat", "airtable_base", "orders_table"):
             layout.prop(self, prop)
 
+
 def get_pref_value(key: str):
     assert bpy.context.preferences
     prefs = bpy.context.preferences.addons[PACKAGE].preferences
-    return getattr(prefs, key)
+    return getattr(prefs, key, "")
